4b9eb37078b4b836d3847a0859c79ee00f994d4f
Kennedy <kennedy.anyinatoe@meltwater.org>
2015-04-03 17:13:15 +0000

    The whole page has been fixed so its now shwing, the form data entry to each event is also working

--- client/hooks.js
+++ client/hooks.js
@@ -20,50 +20,40 @@ AutoForm.hooks({
 AutoForm.hooks({
 	insertBookingForm: {
 		formToDoc: function(doc, ss, formId) {
+			console.log(doc);
 			doc.eventsid = Session.get('eventId');
-			console.log(Session.get('eventId'))
-			// doc.eventsName=Session.get('eventName');
-			// console.log(Session.get('eventName'))
 			console.log(doc);
 			return doc;
 		}
 	}
 
 });
-	// before: {
-	// 	formToDoc: function(doc, ss, formId) {
-	// 		doc.eventsName=Session.get('eventName');
-	// 		console.log(Session.get('eventName'));
-	// 		console.log(doc);
-	// 		return doc;
-	// 	}
-	// }
 
 
-	AutoForm.hooks({
-		insertEventForm: {
-			onSuccess: function(formType, result) {
-				$('#createEvent').modal('hide');
-				return $('#eventSuccess').modal('show');
-			}
-		},
-		insertProfileForm: {
-			onSuccess: function(formType, result) {
-				return $('#profileSuccess').modal('show');
-				Router.go ('events')
-			}
-		},
-		insertEventPictureForm:{
-			onSuccess: function(formType, result) {
-				$('#createEventPicture').modal('hide');
-				return $('#EventPicturesSuccess').modal('show');
-			}
-		},
-		insertBookingForm:{
-			onSuccess: function	(formType, result){
-				$('#bookEvent').modal('hide');
-				return $('#EventBookingSuccess').modal('show');
-			}
+AutoForm.hooks({
+	insertEventForm: {
+		onSuccess: function(formType, result) {
+			$('#createEvent').modal('hide');
+			return $('#eventSuccess').modal('show');
 		}
-	});
+	},
+	insertProfileForm: {
+		onSuccess: function(formType, result) {
+			return $('#profileSuccess').modal('show');
+			Router.go ('events')
+		}
+	},
+	insertEventPictureForm:{
+		onSuccess: function(formType, result) {
+			$('#createEventPicture').modal('hide');
+			return $('#EventPicturesSuccess').modal('show');
+		}
+	},
+	insertBookingForm:{
+		onSuccess: function	(formType, result){
+			$('#bookEvent').modal('hide');
+			return $('#EventBookingSuccess').modal('show');
+		}
+	}
+});
 
--- client/templates/main.js
+++ client/templates/main.js
@@ -3,30 +3,11 @@ Template.events.events({
 	'click [data-book-event]': function (e,t) {
 		var _id = $(e.currentTarget).attr('data-book-event');
 		// console.log(this);
-		Session.set('eventId', this);
+		Session.set('eventId', this._id);
 		// Session.set('eventName', this.placeName);
 	}
 	
 });
-// Template.events.events({
-// 	'click [data-book-event]': function (e,t) {
-// 		var _id = $(e.currentTarget).attr('data-book-event');
-// 		// console.log(this);
-// 		Session.set('eventId', this._id);
-// 		// Session.set('eventName', this.placeName);
-// 	}
-
-// });
-
-// Template.events.events({
-// 	'click #join': function (e,t) {
-// 		var _id = this._id;
-// 		createevents= Events.findOne(_id)
-// 		console.log(this.placeName)
-// 		return this.createevents
-// 	}
-
-// });
 
 Template.modals.helpers({
 	eventName: function (e, t) {
@@ -34,56 +15,4 @@ Template.modals.helpers({
 		// return 'hello'
 	}
 });
-// Template.modals.helpers({
-// 	'eventname': function(e,t){
-// 		var sessionId = Session.get('eventId')
-// 		console.log(sessionId)
-// 		createevents= Events.findOne(_id).placeName
-// 		console.log(createevents)
-// 		return createevents
-// 	}
-// });
 
-// Template.registerHelpers({
-// 	'eventname': function(placeName){
-// 		var _id = this._id;
-// 		createevents= Events.findOne(_id, {placeName: placeName})
-// 		// console.log(this.placeName)
-// 		return createevents
-// 	}
-// });
-
-
-
-
-
-
-
-
-
-// Template.registerHelper({
-// 	'showSelectedEvent': function(_id){
-// 		var selectedEvent= Session.get('selectedEvent');
-// 		// console.log(showSelectedEvent)
-// 		return Events.findOne(selectedEvent)
-// 	}
-// });
-// Template.registerHelpers({
-// 	placeName: function (e,t) {
-// 		var _id = $(e.currentTarget).attr('data-book-event');
-// 		Session.set('eventId', this.placeName);
-// 	}
-
-// });
-// Template.registerHelpers('#bookEvent', function(){
-// 	var placeName = $(e.currentTarget).data().placeName
-// 	return this.placeName
-// });
-
-// Template.events.events({
-// 	'click #bookEvent': function(e,t){
-// 		var placeName = $(e.currentTarget).data().placeName
-// 		return this.placeName
-// 		console.log(this.placeName)
-// 	}
-// })
